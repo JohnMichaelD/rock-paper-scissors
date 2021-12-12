52af85cdd9f7f4d48afc752c740d2f288354a95a
John Darrin <john.m.darrin@gmail.com>
2021-12-12 15:18:23 -0500

    compareSelections function

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+let rockElem = document.querySelector('.rock');
+let paperElem = document.querySelector('.paper');
+let scissorsElem = document.querySelector('.scissors');
+
+let rock = 1;
+let paper = 2;
+let scissors = 3;
+
+let playerSelection = "rock";
+let computerSelection;
+let computerWins = 0;
+let playerWins = 0;
+
+computerPlay();
+playerPlay();
+compareSelections(playerSelection, computerSelection);
+
+// computer randomly chooses rock, paper, or scissors
+function computerPlay() {
+        computer = Math.floor(Math.random() * 3) + 1;
+        if (computer == 1) {
+            computerSelection = "rock";
+        } else if (computer == 2) {
+            computerSelection = "paper";
+        } else if (computer == 3) {
+            computerSelection = "scissors";
+        } else {
+            return 4;
+        }
+    console.log("Computer chose " + computerSelection)
+}
+
+//listen for player input, return choice
+function playerPlay() {
+    rockElem.addEventListener('click', () => {
+        playerSelection = "rock";
+        compareSelections();
+        console.log("Player chose " + playerSelection)
+    })
+    paperElem.addEventListener('click', () => {
+        playerSelection = "paper";
+        console.log("Player chose " + playerSelection)
+    })
+    scissorsElem.addEventListener('click', () => {
+        playerSelection = "scissors";
+        console.log("Player chose " + playerSelection)
+    })
+}
+
+
+// compare the results for each round, maybe switch 
+function compareSelections(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "rock") {
+        return console.log("it's a tie!");
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return console.log("Player Wins with rock!");
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return console.log("Player Wins with paper!")
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return console.log("Player Wins with scissors!");
+    }
+}
+
+console.log(compareSelections(playerSelection, computerSelection));
+
+
 // PSEUDOCODE
 // function computerPlay will randomly return 'rock', 'paper', or 'scissors'
     // let rock = 1
@@ -12,32 +78,4 @@
             // return scissors 
         
 // function 
-
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
-let computerChoice;
-let computer = 0;
-
-
-computerPlay();
-
-function computerPlay() {
-    for (let i = 0; i < 10; i++) {
-        computer = Math.floor(Math.random() * 3) + 1;
-        if (computer == 1) {
-            computerChoice = "rock";
-        } else if (computer == 2) {
-            computerChoice = "paper";
-        } else if (computer == 3) {
-            computerChoice = "scissors";
-        } 
-        console.log(computerChoice);
-    }
-}
-
-
-
-
 
