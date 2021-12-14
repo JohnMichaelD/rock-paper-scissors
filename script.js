20ca69635115a466fe2597c0bdc371a2fb711253
John Darrin <john.m.darrin@gmail.com>
2021-12-14 14:28:30 -0500

    refined output for counts

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ let scissorsElem = document.querySelector('.scissors');
 
 let computerSelection;
 let playerSelection;
-let computerWins = 0;
-let playerWins = 0;
+let computerWinCount = 0;
+let playerWinCount = 0;
 let winner;
 
 //when called, resets scores to 0
 function updateScore() {
-    computerWins = 0;
-    playerWins = 0;
-    return playerWins, computerWins;
+    computerWinCount = 0;
+    playerWinCount = 0;
+    return playerWinCount, computerWinCount;
 }
 
 //compares the scores, first to 5 wins, calls updateScore function
-function winnerResult(playerWins, computerWins) {
-    if (playerWins >= 5) {
+function winnerResult(playerWins, computerWinCount) {
+    if (playerWinCount >= 5) {
         console.log("Player has won first! Play again?");
         updateScore();
-    } else if (computerWins >= 5) {
+    } else if (computerWinCount >= 5) {
         console.log("Computer has beat you! Play again?");
         updateScore();
     } else {
@@ -34,34 +34,34 @@ function playRound(playerSelection, computerSelection) {
         console.log("This round is a tie!");
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
         console.log("Player wins this round with rock!");
-        playerWins++;
-        console.log(playerWins);
-        return winnerResult(playerWins, computerWins);
+        playerWinCount++;
+        console.log("The players current score is: " + playerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("Player wins this round with paper!")
-        playerWins++;
-        console.log(playerWins);
-        return winnerResult(playerWins, computerWins);
+        playerWinCount++;
+        console.log("The players current score is: " + playerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         console.log("Player wins this round with scissors!");
-        playerWins++;
-        console.log(playerWins);
-        return winnerResult(playerWins, computerWins);
+        playerWinCount++;
+        console.log("The players current score is: " + playerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } else if (computerSelection == "rock" && playerSelection == "scissors"){
         console.log("Computer wins this round with rock!");
-        computerWins++;
-        console.log(computerWins);
-        return winnerResult(playerWins, computerWins);
+        computerWinCount++;
+        console.log("The computers current score is: " + computerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } else if (computerSelection == "paper" && playerSelection == "rock"){
         console.log("Computer wins this round with paper!");
-        computerWins++;
-        console.log(computerWins);
-        return winnerResult(playerWins, computerWins);
+        computerWinCount++;
+        console.log("The computers current score is: " + computerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } else if (computerSelection == "scissors" && playerSelection == "paper"){
         console.log("Computer wins this round with scissors!");
-        computerWins++;
-        console.log(computerWins);
-        return winnerResult(playerWins, computerWins);
+        computerWinCount++;
+        console.log("The computers current score is: " + computerWinCount);
+        return winnerResult(playerWinCount, computerWinCount);
     } 
 }
 
