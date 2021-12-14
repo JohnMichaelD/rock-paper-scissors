406750ddfab31fda1f419356e75b28245bf69767
John Darrin <john.m.darrin@gmail.com>
2021-12-14 14:20:56 -0500

    finalized basic code, works

--- script.js
+++ script.js
@@ -6,23 +6,25 @@ let computerSelection;
 let playerSelection;
 let computerWins = 0;
 let playerWins = 0;
-
-game();
-
-function game(){
+let winner;
 
 
+function updateScore() {
+    computerWins = 0;
+    playerWins = 0;
+    return playerWins, computerWins;
+}
 
 
-
-    if (playerWins > computerWins) {
-        console.log("Player wins with " + playerWins + " points!");
-    } else if (computerWins > playerWins) {
-        console.log("Computer wins with " + computerWins + " points!")
-    } else if (computerWins == playerWins) {
-        console.log("tie game")
+function winnerResult(playerWins, computerWins) {
+    if (playerWins >= 5) {
+        console.log("Player has won first! Play again?");
+        updateScore();
+    } else if (computerWins >= 5) {
+        console.log("Computer has beat you! Play again?");
+        updateScore();
     } else {
-        console.log("game function error")
+        return 0;
     }
 }
 
@@ -33,32 +35,32 @@ function playRound(playerSelection, computerSelection) {
         console.log("Player wins this round with rock!");
         playerWins++;
         console.log(playerWins);
-        return playerWins;
+        return winnerResult(playerWins, computerWins);
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         console.log("Player wins this round with paper!")
         playerWins++;
         console.log(playerWins);
-        return playerWins;
+        return winnerResult(playerWins, computerWins);
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         console.log("Player wins this round with scissors!");
         playerWins++;
         console.log(playerWins);
-        return playerWins;
+        return winnerResult(playerWins, computerWins);
     } else if (computerSelection == "rock" && playerSelection == "scissors"){
         console.log("Computer wins this round with rock!");
         computerWins++;
         console.log(computerWins);
-        return computerWins;
+        return winnerResult(playerWins, computerWins);
     } else if (computerSelection == "paper" && playerSelection == "rock"){
         console.log("Computer wins this round with paper!");
         computerWins++;
         console.log(computerWins);
-        return computerWins;
+        return winnerResult(playerWins, computerWins);
     } else if (computerSelection == "scissors" && playerSelection == "paper"){
         console.log("Computer wins this round with scissors!");
         computerWins++;
         console.log(computerWins);
-        return computerWins;
+        return winnerResult(playerWins, computerWins);
     } 
 }
 
@@ -82,19 +84,18 @@ function computerPlay() {
     }
 } 
 
-
-    rockElem.addEventListener('click', () => {
-        playerSelection = "rock";
-        console.log("Player chose " + playerSelection);
-        computerPlay();
-    })
-    paperElem.addEventListener('click', () => {
-        playerSelection = "paper";
-        console.log("Player chose " + playerSelection);
-        computerPlay();
-    })
-    scissorsElem.addEventListener('click', () => {
-        playerSelection = "scissors";
-        console.log("Player chose " + playerSelection);
-        computerPlay();
-    })
+rockElem.addEventListener('click', () => {
+    playerSelection = "rock";
+    console.log("Player chose " + playerSelection);
+    computerPlay();
+})
+paperElem.addEventListener('click', () => {
+    playerSelection = "paper";
+    console.log("Player chose " + playerSelection);
+    computerPlay();
+})
+scissorsElem.addEventListener('click', () => {
+    playerSelection = "scissors";
+    console.log("Player chose " + playerSelection);
+    computerPlay();
+})
